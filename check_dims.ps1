8075847644492fe3e064673ec30517e95c8c828d
toowyred <midcember@outlook.com>
2026-04-21 10:25:32 -0700

    Delete check_dims.ps1

--- check_dims.ps1
+++
@@ -1,8 +0,0 @@
-[void][System.Reflection.Assembly]::LoadWithPartialName('System.Drawing')
-$dir = 'W:\Studio\My Logos\web\elements'
-$files = Get-ChildItem $dir -Filter 'web-background*.png'
-foreach ($f in $files) {
-    $img = [System.Drawing.Image]::FromFile($f.FullName)
-    Write-Host "$($f.Name) : $($img.Width) x $($img.Height)"
-    $img.Dispose()
-}
